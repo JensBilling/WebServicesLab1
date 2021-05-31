46bbc97066043ac3e19b8ad58c7b867d77ad99db
Jens Billing <jensis92@hotmail.com>
2021-05-31 14:49:51 +0200

    Add client handling and dummy F1 json response

---
+++ ServerCore/src/main/java/Formula1Driver.java
@@ -0,0 +1,23 @@
+public class Formula1Driver {
+    private String name;
+    private int age;
+    private boolean isActive;
+
+    public Formula1Driver(String name, int age, boolean isActive) {
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+}
--- ServerCore/src/main/java/Main.java
+++ ServerCore/src/main/java/Main.java
@@ -1,3 +1,14 @@
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -7,6 +18,67 @@ public class Main {
 
         ExecutorService executorService = Executors.newCachedThreadPool();
 
+        try (ServerSocket serverSocket = new ServerSocket(80)){
+            Socket client = serverSocket.accept();
+            executorService.submit(() -> handleConnection(client));
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void handleConnection(Socket client) {
+
+        try {
+            BufferedReader clientInput = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String url = readUrl(clientInput);
+
+            OutputStream clientOutput = client.getOutputStream();
+
+            sendJsonResponse(clientOutput);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void sendJsonResponse(OutputStream clientOutput) throws IOException {
+
+        List<Formula1Driver> listOfDrivers = new ArrayList<>();
+        listOfDrivers.add(new Formula1Driver("Lewis Hamilton", 35, true));
+        listOfDrivers.add(new Formula1Driver("Ayrton Senna", 70, false));
+        listOfDrivers.add(new Formula1Driver("Nikita Mazepin", 21, true));
+
+        Gson gson = new Gson();
+
+        String toJson = gson.toJson(listOfDrivers);
+        byte[] jsonData = toJson.getBytes(StandardCharsets.UTF_8);
+
+        String header = "HTTP/1.1 200 OK\r\nContent-Type: applications/json\r\nContent-length: " + jsonData.length + "\r\n\r\n";
+        byte[] headerData = header.getBytes();
+
+        clientOutput.write(headerData);
+        clientOutput.write(jsonData);
+
+
+    }
+
+    private static String readUrl(BufferedReader clientInput) throws IOException {
+        String url = "";
+        while (true) {
+            String line = clientInput.readLine();
+
+            if  (line.startsWith("GET")) {
+                url = line.split(" ")[1];
+            }
+            if (line == null || line.isEmpty()){
+                break;
+            }
+        }
+        return url;
     }
 }
