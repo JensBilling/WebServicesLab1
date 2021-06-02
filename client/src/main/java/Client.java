c1bc9b57b1535b6b8a05ff7e98e671eedca8485e
Jens Billing <jensis92@hotmail.com>
2021-06-02 17:31:10 +0200

    removed unnecessary dummy-code previously used for testing purposes

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -32,37 +32,6 @@ public class Client {
         }
 
 
-        try {
-            Socket socket = new Socket("localhost", 80);
-
-
-            PrintWriter output = new PrintWriter(socket.getOutputStream());
-
-            output.print("GET /f1 HTTP/1.1\r\n");
-            output.print("Host: localhost\r\n");
-            output.print("\r\n");
-            output.flush();
-
-            BufferedReader inputFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            while (true) {
-                String line = inputFromServer.readLine();
-                if (line == null || line.isEmpty()) {
-                    break;
-                }
-                System.out.println(line);
-            }
-
-            inputFromServer.close();
-            output.close();
-            socket.close();
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
     }
 
     private static void createAndUploadNewUser() {
